a8e6be087be65db0d944c36651929e9d14773320
vhs-cake <vali.kravtsova@gmail.com>
2023-08-29 18:33:50 +0200

    map pins fixed

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -15,13 +15,13 @@ type MapProps = {
 
 const defaultCustomIcon = leaflet.icon({
   iconUrl: URL_MARKER_DEFAULT,
-  iconSize: [40, 40],
+  iconSize: [30, 40],
   iconAnchor: [20, 40],
 });
 
 const currentCustomIcon = leaflet.icon({
   iconUrl: URL_MARKER_CURRENT,
-  iconSize: [40, 40],
+  iconSize: [30, 40],
   iconAnchor: [20, 40],
 });
 
--- src/const.ts
+++ src/const.ts
@@ -48,8 +48,6 @@ export enum CardSort {
   TOP_RATED_FIRST = 'Top rated first',
 }
 
-export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+export const URL_MARKER_DEFAULT = '../public/img/pin.svg';
 
-export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export const URL_MARKER_CURRENT = '../public/img/pin-active.svg';
