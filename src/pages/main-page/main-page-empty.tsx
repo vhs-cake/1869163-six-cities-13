5756a526c81783c9d4ff58e502a27b2945e4ef89
vhs-cake <vali.kravtsova@gmail.com>
2023-08-30 14:36:07 +0200

    empty page fixed

--- src/pages/main-page/main-page-empty.tsx
+++ src/pages/main-page/main-page-empty.tsx
@@ -5,18 +5,18 @@ function MainPageEmpty(): JSX.Element {
   const activeCityName = useAppSelector(activeCityNameSelector);
 
   return (
-    <div className="cities__places-container cities__places-container--empty container">
-      <section className="cities__no-places">
-        <div className="cities__status-wrapper tabs__content">
-          <b className="cities__status">No places to stay available</b>
-          <p className="cities__status-description">
-            We could not find any property available at the moment in{' '}
-            {activeCityName}
-          </p>
-        </div>
-      </section>
-      <div className="cities__right-section">
-        <section className="cities__map map" />
+    <div className="cities">
+      <div className="cities__places-container cities__places-container--empty container">
+        <section className="cities__no-places">
+          <div className="cities__status-wrapper tabs__content">
+            <b className="cities__status">No places to stay available</b>
+            <p className="cities__status-description">
+              We could not find any property available at the moment in{' '}
+              {activeCityName}
+            </p>
+          </div>
+        </section>
+        <div className="cities__right-section"></div>
       </div>
     </div>
   );
